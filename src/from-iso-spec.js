a199660019fb7e4aa2553be6f29e355c3f09220e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-08 23:22:37 -0400

    testing

--- src/from-iso-spec.js
+++ src/from-iso-spec.js
@@ -1,11 +1,13 @@
 'use strict'
 
-
 /* eslint-env mocha */
 const fromIso = require('.')
+const snapshot = require('snap-shot')
 
 describe('from-iso', () => {
-  it('write this test', () => {
-    console.assert(fromIso, 'should export something')
+  it('valid date', () => {
+    const s = '2017-08-09T03:20:10.437Z'
+    const result = fromIso(s)
+    snapshot(result)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 'use strict'
 
+function isoToLocale (str) {
+  if (typeof str !== 'string') {
+    console.error("Sorry, don't know what to do with", str)
+    return
+  }
+  if (!str) {
+    console.error('Empty string is not a date')
+    return
+  }
+  const d = new Date(str)
+  return d.toLocaleString()
+}
 
-// eslint-disable-next-line immutable/no-mutation 
-module.exports = true
-
+// eslint-disable-next-line immutable/no-mutation
+module.exports = isoToLocale
