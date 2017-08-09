3c565c5a51d698dd9d9ddc587cb16e14c8d4822a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-08 23:14:12 -0400

    initial

---
+++ src/from-iso-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const fromIso = require('.')
+
+describe('from-iso', () => {
+  it('write this test', () => {
+    console.assert(fromIso, 'should export something')
+  })
+})
